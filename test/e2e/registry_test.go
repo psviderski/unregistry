ad62a8c2b9acb9283ac393afc0fd8d0045eff28d
Pavel Sviderski <me@psviderski.name>
2025-06-09 19:58:47 +1000

    test: push multi-platform image using local docker with containerd store

--- test/e2e/registry_test.go
+++ test/e2e/registry_test.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/pkg/jsonmessage"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
@@ -154,10 +155,9 @@ func TestRegistryPushPull(t *testing.T) {
 				imageName,
 				registryImage,
 			)
-			require.NoError(
-				t, pushImage(ctx, localCli, registryImage, image.PushOptions{Platform: &ociPlatform}),
-				"Failed to push image '%s' to unregistry", registryImage,
-			)
+			output, err := pushImage(ctx, localCli, registryImage, image.PushOptions{Platform: &ociPlatform})
+			require.NoError(t, err, "Failed to push image '%s' to unregistry", registryImage)
+			assert.NotContains(t, output, "Layer already exists")
 
 			img, _, err = remoteCli.ImageInspectWithRaw(ctx, imageName)
 			require.NoError(t, err, "Pushed image should appear in the remote Docker")
@@ -166,6 +166,11 @@ func TestRegistryPushPull(t *testing.T) {
 			} else {
 				assert.Equal(t, dockerDistribDigest, img.ID, "Image ID should match Docker distribution digest")
 			}
+
+			// Push the same image to test that it doesn't push the same layer again.
+			output, err = pushImage(ctx, localCli, registryImage, image.PushOptions{Platform: &ociPlatform})
+			require.NoError(t, err, "Failed to push image '%s' to unregistry", registryImage)
+			assert.Contains(t, output, "Layer already exists", "Image should not be pushed again if it already exists")
 
 			// Remove the image locally before pulling it back.
 			for _, img := range []string{imageName, registryImage} {
@@ -207,19 +212,115 @@ func TestRegistryPushPull(t *testing.T) {
 					t, dockerLocalDigest, img.ID, "Arbitrary platform pull should match local Docker image digest",
 				)
 			}
+
+			// Remove the image from remote Docker and try to pull it again.
+			_, err = remoteCli.ImageRemove(ctx, imageName, image.RemoveOptions{PruneChildren: true})
+			require.NoError(t, err, "Failed to remove image '%s' from remote Docker", imageName)
+
+			require.ErrorContains(
+				t, pullImage(ctx, localCli, registryImage, image.PullOptions{Platform: platform}),
+				fmt.Sprintf("manifest for %s not found", registryImage),
+				"Pulling image '%s' should fail after removing it from remote Docker", registryImage,
+			)
 		},
 	)
 
-	//t.Run(
-	//	"push/pull multi platform", func(t *testing.T) {
-	//		t.Parallel()
-	//
-	//		// A minimal image that contains only a few platforms.
-	//		imageName := "traefik/whoami:v1.10.0"
-	//		platforms := []string{"linux/amd64", "linux/arm64", "linux/arm/v7"}
-	//	},
-	//)
+	t.Run(
+		"push multi-platform manifest list image (local containerd store)", func(t *testing.T) {
+			if !localDockerUsesContainerdImageStore {
+				t.Skip("Skipping multi-platform image test that requires local Docker to use containerd image store.")
+			}
+			t.Parallel()
 
+			// A minimal image that contains only a few platforms.
+			imageName := "traefik/whoami:v1.10.0"
+			registryImage := fmt.Sprintf("%s/%s", registryAddr, imageName)
+			platforms := []string{"linux/amd64", "linux/arm64", "linux/arm/v7"}
+
+			t.Cleanup(
+				func() {
+					for _, img := range []string{imageName, registryImage} {
+						_, err := localCli.ImageRemove(ctx, img, image.RemoveOptions{PruneChildren: true})
+						if !client.IsErrNotFound(err) {
+							assert.NoError(t, err)
+						}
+					}
+				},
+			)
+
+			// Pull the image locally for all platforms.
+			for _, platform := range platforms {
+				require.NoError(
+					t, pullImage(ctx, localCli, imageName, image.PullOptions{Platform: platform}),
+					"Failed to pull image '%s' locally for platform '%s'", imageName, platform,
+				)
+			}
+
+			summary, err := localCli.ImageList(
+				ctx, image.ListOptions{
+					Filters: filters.NewArgs(
+						filters.Arg("reference", imageName),
+					),
+					Manifests: true,
+				},
+			)
+			require.NoError(t, err, "Failed to list image '%s' locally", imageName)
+			manifests := summary[0].Manifests
+			require.Len(t, manifests, len(platforms), "Image '%s' should have %d manifests", imageName, len(platforms))
+			manifestDigests := make([]string, len(manifests))
+			for i, m := range manifests {
+				require.True(t, m.Available, "Manifest '%s' should be available", m.ID)
+				manifestDigests[i] = m.ID
+			}
+
+			// Tag and push the multi-platform image to unregistry.
+			require.NoError(
+				t, localCli.ImageTag(ctx, imageName, registryImage),
+				"Failed to tag image '%s' as '%s' locally", imageName, registryImage,
+			)
+			output, err := pushImage(ctx, localCli, registryImage, image.PushOptions{}) // all platforms
+			require.NoError(t, err, "Failed to push multi-platform image '%s' to unregistry", registryImage)
+			assert.Equal(t, 5, strings.Count(output, "Pushed"), "Expected 5 layers to be pushed")
+			assert.NotContains(t, output, "Layer already exists")
+
+			// Check the image in remote Docker is the same as in local Docker.
+			remoteSummary, err := remoteCli.ImageList(
+				ctx, image.ListOptions{
+					Filters: filters.NewArgs(
+						filters.Arg("reference", imageName),
+					),
+					Manifests: true,
+				},
+			)
+			require.NoError(t, err, "Failed to list image '%s' in remote Docker", imageName)
+			assert.Equal(t, summary[0].ID, remoteSummary[0].ID, "Image ID should match after pushing to unregistry")
+
+			remoteManifests := remoteSummary[0].Manifests
+			require.Len(
+				t, remoteManifests, len(platforms), "Remote image '%s' should have %d manifests", imageName,
+				len(platforms),
+			)
+			remoteManifestDigests := make([]string, len(remoteManifests))
+			for i, m := range remoteManifests {
+				require.True(t, m.Available, "Remote manifest '%s' should be available", m.ID)
+				remoteManifestDigests[i] = m.ID
+			}
+			assert.ElementsMatch(
+				t, manifestDigests, remoteManifestDigests, "Manifest digests should match after pushing to unregistry",
+			)
+
+			// Push the same image to test that it doesn't push the same layer again.
+			output, err = pushImage(ctx, localCli, registryImage, image.PushOptions{})
+			require.NoError(t, err, "Failed to push multi-platform image '%s' to unregistry", registryImage)
+			assert.Equal(t, 5, strings.Count(output, "Layer already exists"), "Expected 5 layers to already exist")
+			assert.NotContains(t, output, "Pushed", "No new layers should be pushed")
+		},
+	)
+
+	// TODO: push multi-platform index image (local containerd store)
+	// TODO: copy OCI index image and pull a couple platforms
+	// TODO: copy manifest list image and pull a couple platforms
+	// TODO: pull image when the remote one was pulled with containerd enabled but not all platforms are available.
 	// TODO: test pushing an image with digest.
 }
 
@@ -262,20 +363,21 @@ func pullImage(ctx context.Context, cli *client.Client, imageName string, opts i
 	}
 }
 
-func pushImage(ctx context.Context, cli *client.Client, imageName string, opts image.PushOptions) error {
+func pushImage(ctx context.Context, cli *client.Client, imageName string, opts image.PushOptions) (string, error) {
 	if opts.RegistryAuth == "" {
 		opts.RegistryAuth = base64.URLEncoding.EncodeToString([]byte("{}"))
 	}
 
 	respBody, err := cli.ImagePush(ctx, imageName, opts)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer respBody.Close()
 
 	decoder := json.NewDecoder(respBody)
 	errCh := make(chan error, 1)
 
+	var output []string
 	go func() {
 		var jm jsonmessage.JSONMessage
 		for {
@@ -292,15 +394,21 @@ func pushImage(ctx context.Context, cli *client.Client, imageName string, opts i
 				errCh <- fmt.Errorf("push failed for '%s': %s", imageName, jm.Error.Message)
 				return
 			}
+
+			if jm.ID != "" {
+				output = append(output, fmt.Sprintf("%s: %s", jm.ID, jm.Status))
+			} else {
+				output = append(output, jm.Status)
+			}
 		}
 	}()
 
 	for {
 		select {
 		case err = <-errCh:
-			return err
+			return strings.Join(output, "\n"), err
 		case <-ctx.Done():
-			return ctx.Err()
+			return "", ctx.Err()
 		}
 	}
 }
